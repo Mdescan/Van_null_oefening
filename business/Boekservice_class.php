3832559638d18bf55dad4ddf851c39d2f3c693f4
lok117 <lok117@LOK117-01>
2014-08-26 10:18:12 +0200

    created boekservice_class

---
+++ business/Boekservice_class.php
@@ -0,0 +1,10 @@
+<?php
+require_once("data/BoekDAO.php");
+class BoekService {
+    public function getBoekenOverzicht() {
+        $boekDAO = new BoekDAO();
+        $lijst = $boekDAO->getAll();
+        return $lijst;
+    }
+}
+
