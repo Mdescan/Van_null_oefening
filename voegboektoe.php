c3e10d3cc6d5f43105e458a71ac69281c5046acd
lok117 <lok117@LOK117-01>
2014-08-26 11:27:17 +0200

    created voegboektoe.php

---
+++ voegboektoe.php
@@ -0,0 +1,13 @@
+<?php
+require_once("business/Genreservice_class.php");
+require_once("business/Boekservice_class.php");
+if ($_GET["action"] == "process") {
+    $boekSvc = new BoekService();
+    $boekSvc->voegNieuwBoekToe($_POST["txtTitel"], $_POST["selGenre"]);
+    header("location: toonalleboeken.php");
+    exit(0);
+} else {
+    $genreSvc = new GenreService();
+    $genreLijst = $genreSvc->getGenresOverzicht();
+    include("presentation/nieuwboekform.php");
+}
