cbe2977c67608b29b4a9e8fbb0090e1787ca6e10
lok117 <lok117@LOK117-01>
2014-08-26 10:18:54 +0200

    changed test file to test out the boekservice class

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
-require_once("data/GenreDAO.php");
-$dao = new GenreDAO();
-$lijst = $dao->getAll();
+require_once("business/Boekservice_class.php");
+$boekSvc = new BoekService();
+$lijst = $boekSvc->getBoekenOverzicht();
 print("<pre>");
 print_r($lijst);
 print("</pre>");
