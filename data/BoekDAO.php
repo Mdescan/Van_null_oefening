9db651ce37bbe952b39394d4011fc72888659dea
lok117 <lok117@LOK117-01>
2014-08-27 09:16:10 +0200

    adding exception

--- data/BoekDAO.php
+++ data/BoekDAO.php
@@ -34,6 +34,10 @@ class BoekDAO {
     }
     
     public function create($titel, $genreId) {
+        $bestaandBoek = $this->getByTitel($titel);
+        if (isset($bestaandBoek)){ 
+            throw new TitelBestaatException();
+        }
         $sql = "insert into mvc_boeken (titel, genreid) values ('" . $titel . "', " . $genreId . ")";
         $dbh = new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME,DBConfig::$DB_PASSWORD);
         $dbh->exec($sql);
@@ -58,5 +62,22 @@ class BoekDAO {
         $dbh = new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME,DBConfig::$DB_PASSWORD);
         $dbh->exec($sql);
         $dbh = null;
+    }
+    
+    public function getByTitel($titel) {
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME,DBConfig::$DB_PASSWORD);
+        $sql = "select mvc_boeken.id as boekid, titel, genreid, omschrijving
+        from mvc_boeken, mvc_genres where genreid = mvc_genres.id
+        and titel = '" . $titel . "'";
+        $resultSet = $dbh->query($sql);
+        $rij = $resultSet->fetch();
+        if (!$rij) {
+            return null;
+        } else {
+            $genre = Genre::create($rij["genreid"], $rij["omschrijving"]);
+            $boek = Boek::create($rij["boekid"], $rij["titel"], $genre);
+            $dbh = null;
+            return $boek;
+        }
     }
 }
