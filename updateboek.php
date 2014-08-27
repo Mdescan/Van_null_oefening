64b24f5821fc7dae69e9ecf25ea3a6805b1d8c11
lok117 <lok117@LOK117-01>
2014-08-27 11:30:37 +0200

    adding titelbestaat exception

--- updateboek.php
+++ updateboek.php
@@ -3,15 +3,23 @@ require_once("business/Genreservice_class.php");
 require_once("business/Boekservice_class.php");
 require_once ("exceptions/TitelbestaatException.php");
 if (isset($_GET["action"]) && $_GET["action"] == "process") {
-    $boekSvc = new BoekService();
-    $boekSvc->updateBoek($_GET["id"], $_POST["txtTitel"], $_POST["selGenre"]);
-    header("location: toonalleboeken.php");
-    exit(0);
+    try{
+        $boekSvc = new BoekService();
+        $boekSvc->updateBoek($_GET["id"], $_POST["txtTitel"], $_POST["selGenre"]);
+        header("location: toonalleboeken.php");
+        exit(0);
+    }  catch (TitelBestaatException $tbe){
+        header("location: updateboek.php?id=".$_GET["id"]."&error=titleexists");
+        exit(0);
+    }
 } else {
     $genreSvc = new GenreService();
     $genreLijst = $genreSvc->getGenresOverzicht();
     $boekSvc = new BoekService();
     $boek = $boekSvc->haalBoekOp($_GET["id"]);
+    if(isset($_GET["error"])){
+        $error = $_GET["error"];       
+    }
     include("presentation/updateboekform.php");
 }
 
